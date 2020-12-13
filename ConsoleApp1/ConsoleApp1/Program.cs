1bbf7cf35505a346995d0a3da2cc8e095df5c2e0
npasha616 <75905186+npasha616@users.noreply.github.com>
2020-12-13 16:22:41 +0530

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Code was added in Git Hub
+            //Code was edited in Git Hub
         }
     }
 }
